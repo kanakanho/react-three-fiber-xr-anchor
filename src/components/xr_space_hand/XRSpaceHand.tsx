356c73f39278ca6b6cd8d2786cfedba4b25b2205
kanakanho <kahoshibakichi@gmail.com>
2024-10-10 17:55:27 +0900

    fix: テキストの表示でHtmlを使うのをやめる

--- src/components/xr_space_hand/XRSpaceHand.tsx
+++ src/components/xr_space_hand/XRSpaceHand.tsx
@@ -1,4 +1,4 @@
-import { Html } from '@react-three/drei';
+import { Text } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { useXRAnchor, useXRInputSourceEvent, useXRInputSourceState, XRSpace } from '@react-three/xr';
 import React from 'react';
@@ -42,13 +42,11 @@ const XRSpaceHand = ({ setHandPosition }: Props) => {
 
   return (
     <XRSpace space={anchor.anchorSpace}>
-      <Html position={[0, 1, 0]}>
-        <div style={{ backgroundColor: 'white' }}>
-          <p style={{ fontSize: '3rem' }}>{handState?.object?.position.x}</p>
-          <p style={{ fontSize: '3rem' }}>{handState?.object?.position.y}</p>
-          <p style={{ fontSize: '3rem' }}>{handState?.object?.position.z}</p>
-        </div>
-      </Html>
+      <group>
+        <Text position={new Vector3(0, 1, 0)} fontSize={0.1} color="white">
+          {handState?.object?.getWorldPosition(outHandPosition).toArray().join(',')}
+        </Text>
+      </group>
     </XRSpace>
   );
 };
