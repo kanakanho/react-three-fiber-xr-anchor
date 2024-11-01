0643ca1950a7804089a34a2d0a5016c19abc450d
kanakanho <kahoshibakichi@gmail.com>
2024-11-01 18:24:54 +0900

    fix: depthAPIの対応、表示の強化

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ import styles from './App.module.css';
 import XRButton from './components/xr_button/XRButton';
 import XRIndex from './components/xr_index/XRIndex';
 
-const store = createXRStore();
+const store = createXRStore({
+  depthSensing: true,
+  hand: {
+    model: false,
+  },
+  controller: {
+    model: false,
+  },
+  frameBufferScaling: 'high',
+});
 
 function App() {
   return (
